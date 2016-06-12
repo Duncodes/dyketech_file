3c4f5687131642ae2a5da210caa33071951dd54c
duncan <duncangitonga292@gmail.com>
2016-06-12 18:32:50 +0300

    test add

--- test.py
+++ test.py
@@ -4,13 +4,14 @@ import unittest
 import tempfile
 class FlaskrTestCase(unittest.TestCase):
     def setUp(self):
-        self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+        self.db_fd, application.app.config['DATABASE'] = tempfile.mkstemp()
         application.app.config['TESTING'] = True
-        self.app = flaskr.app.test_client()
-        with application.app.app_context():
-            flaskr.init_db()
+        self.app = application.app.test_client()
     def tearDown(self):
         os.close(self.db_fd)
-        os.unlink(flaskr.app.config['DATABASE'])
+        os.unlink(application.app.config['DATABASE'])
+    def test_empty_db(self):
+        rv=self.app.get('/')
+        assert b'Dyketech personal cloud' in rv.data
 if __name__ == '__main__':
     unittest.main()
